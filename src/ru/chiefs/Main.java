63900a3c400a311db8b3d09468691c464857f998
Pussymonsta <fominok@hotmail.com>
2014-09-17 01:04:44 +0400

    Basic project structure. OMFG THE BEST STARTUP OF THE MILLENIUM OH SHIET BILLIONS OF DOLLARS AND BITCHEZ FOR CHIEFS

---
+++ src/ru/chiefs/Main.java
@@ -0,0 +1,10 @@
+package ru.chiefs;
+
+/**
+ * Created by yolo on 17.09.14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("ThA BIEST SHIET BEGINEN TERE");
+    }
+}
