6b56dfebb73c2a17dc6638f218566a4f32b5d50d
Pussymonsta <fominok@hotmail.com>
2014-09-17 12:31:10 +0400

    Tryin into callbacks

--- src/ru/chiefs/Main.java
+++ src/ru/chiefs/Main.java
@@ -1,11 +1,42 @@
 package ru.chiefs;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by yolo on 17.09.14.
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("ThA BIEST SHIET BEGINEN TERE");
-        System.out.print("Y U DON COMMITIN FUCK IDEA");
+        System.out.println("ThA BIEST SHIET BEGINEN TERE");
+        System.out.println("Y U DON COMMITIN FUCK IDEA"); // Lol i forgot to make master branch
+
+        //event test starts there
+        //i will delete it later
+        //maybe
+        //на самом деле это дерьмо должно было запускаться с той стороны
+
+        Parent parent = new Parent();
+        FunctionCaller caller = new FunctionCaller(parent);
+        caller.tryinShit(); //Это что-то типо произошло на стороне, оттуда берем обратный вызов. МАКЕТ ХУЛИ
+    }
+
+}
+
+class FunctionCaller {
+    private Runnable parentClass;
+    public FunctionCaller(Runnable parent) {
+        parentClass = parent;
+    }
+    public void tryinShit() {
+        parentClass.run();
+    }
+}
+
+class Parent implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("callback received from child Function caller. Message from parent ");
     }
 }
